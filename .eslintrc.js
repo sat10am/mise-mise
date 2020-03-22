c9d8bd1f4d15ccf742159b84f1ab1dc338b8516c
amorfati0310 <lyhy0310@gmail.com>
2020-03-22 21:45:46 +0900

    add: eslint-import-reslover-webpack -> fix import extension
    여전히 @ alias 설정은 webpack 설정에서 못 참조를 하는 것 같다.
    - jsconfig.json에 @ alias 설정을 넣어줘도 그대로여서 당분간 import/no-unresolved는 off로 진행
    - devDependencies 구분되게 수정
    [Ticket: 05]

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,34 @@ const ERROR = 2;
 
 module.exports = {
   extends: ['react-app', 'airbnb', 'prettier'],
+  plugins: ['import', 'react-hooks'],
   rules: {
     'react/prefer-stateless-function': OFF,
     'react/jsx-filename-extension': OFF,
     'react/jsx-one-expression-per-line': OFF,
     'import/no-extraneous-dependencies': OFF,
     // TODO Webpack 설정을 잡아서 lint 에러가 안나게 해야 함 임시 방편
+    // plugin 설정을 해줬는데 -> alias는 못 잡는 이슈가 있다.
     'import/no-unresolved': OFF,
-    'import/extensions': OFF,
+    'react-hooks/rules-of-hooks': ERROR, // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': WARNING, // Checks effect dependencies
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   env: {
     browser: true,
     jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'config-overrides.js',
+      },
+    },
   },
 };
