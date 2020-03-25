12d0140caaed64cb9241521f170b009a0bdced53
amorfati0310 <lyhy0310@gmail.com>
2020-03-26 02:40:31 +0900

    Refactor: make api mise module
    [Ticket: 2]

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,3 @@
-import axios from 'axios';
+import MiseApi from '@/api/modules/mise';
 
-const defaultParams = {
-  params: {
-    ServiceKey: process.env.REACT_APP_MISE_SERVICE_KEY,
-    _returnType: 'json',
-  },
-};
-
-const MiseApi = (() => {
-  const instance = axios.create({
-    baseURL: `http://openapi.airkorea.or.kr/openapi/services/rest/UlfptcaAlarmInqireSvc`,
-  });
-  instance.interceptors.request.use(
-    config => {
-      return Object.assign(config, defaultParams);
-    },
-    error => {
-      return Promise.reject(error.response);
-    },
-  );
-  instance.interceptors.response.use(
-    config => {
-      return config;
-    },
-    error => {
-      return Promise.reject(error.response);
-    },
-  );
-  return instance;
-})();
-
-export default {
-  get: params => {
-    MiseApi.get('', { params });
-  },
-};
+export default { MiseApi };
---
+++ src/api/modules/mise.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const defaultParams = {
+  params: {
+    ServiceKey: process.env.REACT_APP_MISE_SERVICE_KEY,
+    _returnType: 'json',
+  },
+};
+
+const MiseApi = (() => {
+  const instance = axios.create({
+    baseURL: `${process.env.REACT_APP_MISE_BASE_URL}/UlfptcaAlarmInqireSvc`,
+  });
+  instance.interceptors.request.use(
+    config => {
+      return Object.assign(config, defaultParams);
+    },
+    error => {
+      return Promise.reject(error.response);
+    },
+  );
+  instance.interceptors.response.use(
+    config => {
+      return config;
+    },
+    error => {
+      return Promise.reject(error.response);
+    },
+  );
+  return instance;
+})();
+
+export default {
+  get: params => {
+    MiseApi.get('', { params });
+  },
+};
